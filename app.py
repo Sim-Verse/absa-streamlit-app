1a2d688e062e3797cb4b0b1c7647d4b32b8ac937
Sim-Verse <s1imrbb@gmail.com>
2025-04-21 09:12:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if uploaded_file:
 
             if st.button("🔍 Run ABSA on Reviews"):
                 results = []
-                for review in df['review']:
+                for review in df['reviews']:
                     result = absa_pipeline(review)
                     results.append(result)
 
